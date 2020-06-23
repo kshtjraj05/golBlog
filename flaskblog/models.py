6690e7b244325f85ed3bf86bb4e39c6deaf2550a
imbharatk <khannabharat001@gmail.com>
2020-06-23 12:44:49 +0530

    Added Tour-item Navbar

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -73,6 +73,7 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(120), unique=True, nullable = False)
     image_file = db.Column(db.String(120), nullable = False, default= 'default.jpeg')
     password = db.Column(db.String(60), nullable = False)
+    can_view_tour = db.Column(db.Boolean, default=True, nullable=False)
 
     posts=db.relationship('Post', backref='author', lazy =True)
     
